12ec15e2f154b0707a09bced868e18aaab7561fb
jeffrey <908202572@qq.com>
2022-10-04 13:26:23 +0800

    引入 prototype

---
+++ src/dom-jquery-style-prototype.js
@@ -0,0 +1,43 @@
+window.jQuery = function (selectorOrArray) {
+    // return 的结果叫做「jQuery 对象」。jQuery 对象中的方法都是闭包，闭包可以访问外层函数的内部变量 elements 
+    let elements = null
+    if (typeof selectorOrArray === 'string') {
+        elements = document.querySelectorAll(selectorOrArray)
+    } else if (selectorOrArray instanceof Array) {
+        elements = selectorOrArray
+    }
+    let api = Object.create(jQuery.prototype)
+    Object.assign(api, {
+        _oldApi: null,
+        elements: elements
+    })
+    return api
+}
+jQuery.prototype = {
+    find(selector) {
+        let array = []
+        this.each((ele, i) => {
+            array = array.concat(Array.from(ele.querySelectorAll(selector)))
+        })
+        let newApi = jQuery(array)
+        newApi._oldApi = this
+        return newApi
+    },
+    end() {
+        return this._oldApi
+    },
+    addClass(className) {
+        this.each((ele, i) => {
+            ele.classList.add(className)
+        })
+        return this
+    },
+    each(fn) {
+        for (let i = 0; i < this.elements.length; i++) {
+            fn.call(null, this.elements[i], i) // call null 的好处是什么呢？
+        }
+    },
+    print() {
+        console.log(this.elements)
+    }
+}
